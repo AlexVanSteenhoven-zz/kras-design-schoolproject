7ae8515dbf5c946bc25d8055a695a801df5475d6
Alex van Steenhoven <37883414+AlexVanSteenhoven@users.noreply.github.com>
2020-03-05 13:51:31 +0100

    changed header.html

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <?php require_once ('includes/header.html')?>
+        <?php require_once('includes/index_header.html') ?>
         <title>Document</title>
     </head>
     <body>
