745cd0174e6afcef12ea3a32923d5ef50c33e4c6
Alex van Steenhoven <37883414+AlexVanSteenhoven@users.noreply.github.com>
2020-03-19 13:47:33 +0100

    fixed project (AGAIN)

--- pages/designs.php
+++ pages/designs.php
@@ -5,8 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <?php require_once('../../include/config.php'); ?>
-    <title><?php echo SITE_TITLE?> || News</title>
+    <?php require_once('../globals/config.php'); ?>
+    <title><?php echo SITE_TITLE?> || Designs</title>
 </head>
 <body>
 <!--    Import navigation bar    -->
--- pages/hosting.php
+++ pages/hosting.php
@@ -5,7 +5,7 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <?php require_once('../../include/config.php'); ?>
+        <?php require_once('../globals/config.php'); ?>
         <title><?php echo SITE_TITLE?> || Hosting</title>
     </head>
     <body>
