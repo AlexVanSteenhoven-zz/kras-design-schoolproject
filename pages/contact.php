65cac736da45b4ded8f3bd142742cdf620c9aeb9
Alex van Steenhoven <37883414+AlexVanSteenhoven@users.noreply.github.com>
2020-03-26 22:35:55 +0100

    finished contact form

--- pages/contact.php
+++ pages/contact.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../assets/css/layouts/contact.css">
     <?php require_once('../globals/config.php'); ?>
     <?php require_once('../includes/index_header.html'); ?>
     <title><?php echo SITE_TITLE?> || Contact</title>
@@ -14,7 +15,31 @@
 <?php require_once('../partials/navbar.html') ?>
 
 <!--    Body goes here    -->
+<div><h1>Neem contact met ons op</h1></div>
+<form action="contact.php" method="post" class="container">
+    <div class="name div">
+        <div><h3>Geef uw naam op*</h3></div>
+        <div><input type="text" required placeholder="Naam" name="name"></div>
+    </div>
 
+    <div class="email div">
+        <div><h3>Geef uw E-mail op*</h3></div>
+        <div><input type="text"  required placeholder="E-mail" name="email"></div>
+    </div>
+
+    <div class="tel div">
+        <div><h3>Geef uw Telefoonnummer op</h3></div>
+        <div><input type="text" placeholder="Telefoonnummer" name="tel"></div>
+    </div>
+
+    <div class="text div">
+        <h3>Wat heeft u te vertellen?*</h3>
+        <textarea name="text" required placeholder="Uw vraag/opmerking" id="" cols="28" rows="9"></textarea>
+    </div>
+    <div class="submit div">
+        <button class="submit div">Verzend die shit</button>
+    </div>
+</form>
 
 <!--    Import footer    -->
 <?php require_once('../partials/footer.html') ?>
