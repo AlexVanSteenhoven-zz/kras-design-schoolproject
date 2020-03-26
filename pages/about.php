bc7e70fadd21f5923f6d1b51e4359de8444d3a9b
Alex van Steenhoven <37883414+AlexVanSteenhoven@users.noreply.github.com>
2020-03-26 22:48:21 +0100

    finished about page

--- pages/about.php
+++ pages/about.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php require_once('../globals/config.php'); ?>
     <?php require_once('../includes/index_header.html'); ?>
+    <link rel="stylesheet" href="../assets/css/layouts/over_ons.css">
     <title><?php echo SITE_TITLE?> || About</title>
 </head>
 <body>
@@ -14,7 +15,23 @@
 <?php require_once('../partials/navbar.html') ?>
 
 <!--    Body goes here    -->
-
+<div class="container">
+    <div class="title">
+        <h1>Over Kras Design</h1>
+        <img src="../assets/img/owners.png" alt="Eigenaren">
+    </div>
+    <div class="body">
+        <p>
+            Wij zijn Jan en Freek Kras en samen met ons team helpen wij bedrijven in Nederland en België met
+            het bouwen en ontwerpen van nieuwe websites en analyseren en optimaliseren van bestaande
+            websites. Ondertussen hebben we ruim 350 websites gemaakt en geoptimaliseerd. Dit doe ik niet
+            alleen maar samen met een team van webdesigners en grafisch vormgevers. We willen ons
+            onderscheiden door een hoge kwaliteit te leveren voor een lage prijs, met persoonlijk contact als
+            basis. Onze focus ligt op het optimaliseren van bestaande websites met als doel succes voor onze
+            klant. We houden van transparantie en staan voor open en eerlijke communicatie.
+        </p>
+    </div>
+</div>
 
 <!--    Import footer    -->
 <?php require_once('../partials/footer.html') ?>
